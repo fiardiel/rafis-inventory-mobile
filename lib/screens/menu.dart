8c68c038c699210f44fe7d3f8d096c312b7bb8b5
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2023-11-11 02:29:18 +0700

    Modified assignment 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:rafis_inventory/screens/additem_form.dart';
 // import drawer widget
 import 'package:rafis_inventory/widgets/left_drawer.dart';
 import 'package:rafis_inventory/widgets/shop_card.dart';
 
-class ShopItem {
-  final String name;
-  final IconData icon;
-  final Color color;
-  ShopItem(this.name, this.icon, this.color);
-}
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -74,60 +67,6 @@ class MyHomePage extends StatelessWidget {
                 }).toList(),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {Key? key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.color,
-      child: InkWell(
-        // Area responsive to touch
-        onTap: () {
-          // Show SnackBar when clicked
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("You pressed the ${item.name} button!")));
-
-          // Navigate to the appropriate route (depending on the button type)
-          if (item.name == "Add Item") {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ShopFormPage(),
-                ));
-          }
-        },
-        child: Container(
-          // Container to hold Icon and Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
           ),
         ),
       ),
